84a78a7700c2f32f3afe359cafc3e9fb51987c54
Dmitry <kashlevdmitry@gmail.com>
2019-02-08 22:08:25 +0300

    Complete MyXylophone Application

---
+++ MyXylophone/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  MyXylophone
+//
+//  Created by Madwor1d3 on 08/02/2019.
+//  Copyright © 2019 Madwor1d3. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var audioPlayer: AVAudioPlayer!
+    let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func notePressed(_ sender: UIButton) {
+        playSound(soundFileName: soundArray[sender.tag - 1])
+    }
+    
+    
+    func playSound(soundFileName: String) {
+        let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: "wav")
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
+        } catch {
+            print(error)
+        }
+        audioPlayer.play()
+    }
+}
+
