02ddeb2b1f353ada949ffd20e8ad4b54d7420158
Dmitry <kashlevdmitry@gmail.com>
2019-02-09 15:51:41 +0300

    add 2x & 3x background and new borders

---
+++ MyXylophone/BorderButton.swift
@@ -0,0 +1,20 @@
+//
+//  BorderButton.swift
+//  MyXylophone
+//
+//  Created by Madwor1d3 on 09/02/2019.
+//  Copyright © 2019 Madwor1d3. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.borderWidth = 2.0
+        layer.borderColor = UIColor.white.cgColor
+        layer.cornerRadius = 30
+    }
+
+}
