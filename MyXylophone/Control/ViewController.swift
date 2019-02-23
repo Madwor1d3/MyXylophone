ef2c985a8dbf11f634a6985eb052db72a1cbaeba
Dmitry <kashlevdmitry@gmail.com>
2019-02-23 19:44:50 +0300

    XyloImage added

--- MyXylophone/Control/ViewController.swift
+++ MyXylophone/Control/ViewController.swift
@@ -11,29 +11,37 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    
     var audioPlayer: AVAudioPlayer!
     let soundArray = ["note1", "note2", "note3", "note4", "note5", "note6", "note7"]
     
+    
     override var preferredStatusBarStyle: UIStatusBarStyle {
+        
         return .lightContent
     }
 
     override func viewDidLoad() {
+        
         super.viewDidLoad()
     }
 
     @IBAction func notePressed(_ sender: UIButton) {
+        
         playSound(soundFileName: soundArray[sender.tag - 1])
     }
     
     
     func playSound(soundFileName: String) {
+        
         let soundURL = Bundle.main.url(forResource: soundFileName, withExtension: "wav")
+        
         do {
             audioPlayer = try AVAudioPlayer(contentsOf: soundURL!)
         } catch {
             print(error)
         }
+        
         audioPlayer.play()
     }
 }
--- MyXylophone/View/BorderButton.swift
+++ MyXylophone/View/BorderButton.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class BorderButton: UIButton {
 
+    
     override func awakeFromNib() {
+        
         super.awakeFromNib()
-        layer.borderWidth = 2.0
-        layer.borderColor = UIColor.white.cgColor
-        layer.cornerRadius = 30
+        
+        layer.borderWidth   =   2.0
+        layer.borderColor   =   UIColor.white.cgColor
+        layer.cornerRadius  =   30
     }
 
 }
